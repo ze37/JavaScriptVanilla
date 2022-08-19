d741302e722c983d5982b5f2460cd808bd83764e
ze37 <josealvess6154@gmail.com>
2022-08-19 18:21:17 -0300

    Estudo sobre Estrutura condicionais

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let age = 18;
+if(age > 18){
+    console.log("Ele é maior de idade!");
+}else if(age == 18){
+    console.log("Ele tem esatamente 18 anos");
+}else{
+    console.log("Ele é menor de idade");
+}
