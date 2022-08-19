3bb3061b123593d9171a3b94ff97ec5e43fde21b
ze37 <josealvess6154@gmail.com>
2022-08-19 13:34:55 -0300

    Estudo sobre Alertas caixas de dialogos

---
+++ aula3/index.js
@@ -0,0 +1,27 @@
+//Caixa de alerta .
+
+/* alert("esse é um alerta") */
+
+// Caixa de dialogo
+
+/* confirm("Deseja mesmo apagar este dado ? \m Esse dado sera excluido!"); */
+
+// Caixa de prompt
+/*prompt("Qual a sua data de nascimento ", "dd/mm/yy")*/
+
+let numberA = 3;
+let numberB = 4;
+let numberC = '6';
+let stringA = " Estudo sobre java";
+
+// Operadores de aritimetricos
+console.log(numberA += 3);
+for(let i=10; i>5; i++){
+    console.log(i);
+}
+
+// Operadores de igualdade
+console.log(numberA <= numberB);
+
+
+// Operadores logicos
