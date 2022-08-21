3d3f9c0e21938f737e8de0738e91b0a722950d1d
ze37 <josealvess6154@gmail.com>
2022-08-21 09:07:57 -0300

    estudo sobre objetos com java script

---
+++ aula7/java.js
@@ -0,0 +1,16 @@
+let pesso1={
+    nome: 'ze alves'
+};
+
+let pesso2={
+    nome: "gana",
+    idade: 23,
+    status: false,
+    array: ["profissional", "hacker", "loja"],
+    work: function (){
+        alert("work");
+    },
+    support: pesso1
+};
+
+console.log (pesso1.nome);
--- javascript.js
+++
@@ -1,8 +0,0 @@
-
-let somaArrow = (x, y) => {
-    return x / y;
-};
-console.log(somaArrow(8, 2));
-
-let somaArrow2 = (y, x) => y + x;
-console.log(somaArrow2(9,23));
